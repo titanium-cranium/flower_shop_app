3ca3e2a995163b5c6038d57cc93f99c77f1da327
Brendan O'Brien <titaniumcranium@icloud.com>
2021-02-05 01:30:18 +1100

    completed OrderProcessor and spec

--- lib/order_processor.rb
+++ lib/order_processor.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class OrderProcessor
   attr_reader :order, :flower
 
@@ -6,20 +8,18 @@ class OrderProcessor
     @flower = flower
   end
 
-  # def process
-  #   shipment = create_shipment
-  #   bundles_with_cost = total_cost(shipment)
-  #
-  #   { number: order.number,
-  #     flower: flower.code,
-  #     total: bundles_with_cost[:total],
-  #     bundles: bundles_with_cost[:bundles]
-  #   }
-  # end
+  def process
+    shipment = create_shipment
+    bundles_with_cost = total_cost(shipment)
+    { number: order.number,
+      flower: flower.code,
+      total: bundles_with_cost[:total],
+      bundles: bundles_with_cost[:bundles] }
+  end
 
   def create_shipment
-    bundle_sizes = flower.bundles.collect { |bundle| bundle["number"]}.sort.reverse
-    shipment = ShipmentGenerator.generate_shipment(number: order.number, sizes: bundle_sizes)
+    bundle_sizes = flower.bundles.collect { |bundle| bundle[:number] }
+    ShipmentGenerator.generate_shipment(number: order.number, sizes: bundle_sizes)
   end
 
   def total_cost(shipment)
@@ -29,13 +29,12 @@ class OrderProcessor
       bundles << bundle
     end
     total = bundles.reduce(0) { |sum, bundle| sum + bundle[:price] }
-    {total: total.round(2), bundles: bundles}
+    { total: total.round(2), bundles: bundles }
   end
 
   def bundle_cost(bundle)
-    bundle_cost = flower[:bundles].select { |b| b[:number] == bundle[:size] }.first[:price]
+    bundle_cost = flower.bundles.select { |b| b[:number] == bundle[:size] }.first[:price]
     bundle[:price] = bundle_cost * bundle[:bunches]
     bundle
   end
-
 end
--- spec/unit/order_processor_spec.rb
+++ spec/unit/order_processor_spec.rb
@@ -1,27 +1,47 @@
 # frozen_string_literal: true
 
 require_relative '../../lib/order_processor'
+require_relative '../../lib/flower'
+require_relative '../../lib/order'
 
 RSpec.describe OrderProcessor do
-  describe '#total_cost' do
-    let(:order) do
-      {
-        number: 13,
-        code: 'T58'
-      }
-    end
+  describe '#process' do
+    let(:order) { Order.new(number: 13, code: 'T58') }
 
     let(:flower) do
-      {
-        "name": 'Tulips',
-        "code": 'T58',
-        "bundles": [
-          { "number": 3, "price": 5.95 },
-          { "number": 5, "price": 9.95 },
-          { "number": 9, "price": 16.99 }
-        ]
-      }
+      Flower.new(name: 'Tulips', code: 'T58', bundles: [
+                   { "number": 3, "price": 5.95 },
+                   { "number": 5, "price": 9.95 },
+                   { "number": 9, "price": 16.99 }
+                 ])
     end
+
+    let(:op) do
+      OrderProcessor.new(order: order, flower: flower)
+    end
+
+    let(:result) do
+      { number: 13, flower: 'T58', "total": 25.85, "bundles": [{ "bunches": 2, "size": 5, "price": 19.90 }, { "bunches": 1, "size": 3, "price": 5.95 }] }
+    end
+
+    describe 'given an order and flower' do
+      it 'processes the order' do
+        expect(op.process).to eq(result)
+      end
+    end
+  end
+
+  describe '#total_cost' do
+    let(:order) { Order.new(number: 13, code: 'T58') }
+
+    let(:flower) do
+      Flower.new(name: 'Tulips', code: 'T58', bundles: [
+                   { "number": 3, "price": 5.95 },
+                   { "number": 5, "price": 9.95 },
+                   { "number": 9, "price": 16.99 }
+                 ])
+    end
+
     let(:shipment) do
       { shipment: [{ size: 5, bunches: 2 }, { size: 3, bunches: 1 }], remainder: 0 }
     end
@@ -30,9 +50,8 @@ RSpec.describe OrderProcessor do
       OrderProcessor.new(order: order, flower: flower)
     end
 
-    let (:result) do
-      { "total":  25.85, "bundles": [{"bunches": 2, "size": 5, "price": 19.90 }, {"bunches": 1, "size": 3 , "price": 5.95 }]}
-
+    let(:result) do
+      { "total": 25.85, "bundles": [{ "bunches": 2, "size": 5, "price": 19.90 }, { "bunches": 1, "size": 3, "price": 5.95 }] }
     end
 
     context 'given a shipment with multiple bundles' do
@@ -43,21 +62,13 @@ RSpec.describe OrderProcessor do
   end
 
   describe '#bundle_cost' do
-    let(:order) do
-      {
-        number: 10,
-        code: 'R12'
-      }
-    end
+    let(:order) { Order.new(number: 13, code: 'T58') }
+
     let(:flower) do
-      {
-        name: 'Roses',
-        code: 'R12',
-        bundles: [
-          { number: 5, price: 6.99 },
-          { number: 10, price: 12.99 }
-        ]
-      }
+      Flower.new(name: 'Roses', code: 'R12', bundles: [
+                   { "number": 5, "price": 6.99 },
+                   { "number": 10, "price": 12.99 }
+                 ])
     end
     let(:bundle) do
       { size: 10, bunches: 1 }
