9582de662b8af6c2e3cf1d60a74b7ce4f9cf5e08
Brendan O'Brien <titaniumcranium@icloud.com>
2021-02-05 04:08:49 +1100

    updated flower_shop_app and spec
    removed JSON requirements as unnecessary

--- lib/flower_shop_app.rb
+++ lib/flower_shop_app.rb
@@ -1,9 +1,23 @@
 require 'order'
 require 'flower'
+require 'shipment_generator'
+require 'order_processor'
 
 class FlowerShopApp
   def self.call(orders, flowers)
-    @orders = orders.map { |order| Order.new(number: order[0], code: order[1]) }
-    @flowers = flowers.map { |flower| Flower.new(name: flower['name'], code: flower['code'], bundles: flower['bundles']) }
+    Money.locale_backend = nil
+    Money.rounding_mode=BigDecimal::ROUND_HALF_UP
+    # orders = orders.map { |order| Order.new(number: order[0], code: order[1]) }
+    # flowers = flowers.map { |flower| Flower.new(name: flower['name'], code: flower['code'], bundles: flower['bundles']) }
+    process_orders(orders, flowers)
+  end
+
+  def self.process_orders(orders, flowers)
+    receipt = []
+    orders.each do |order|
+      flower = flowers.select { |f| f.code == order.code }.first
+      receipt << OrderProcessor.new(order: order, flower: flower).process
+    end
+    receipt
   end
 end
--- spec/integration/flower_shop_app_spec.rb
+++ spec/integration/flower_shop_app_spec.rb
@@ -1,27 +1,42 @@
-require 'json'
-require 'flower_shop_app'
+# frozen_string_literal: true
 
+require 'json'
+require_relative '../../lib/flower_shop_app'
 
 RSpec.describe 'integration' do
-
-  let(:order) {['10 R12', '15 L09', '13 T58']}
-
-  let(:expected_result_json) {
-    <<~JSON
-      [
-        {"number":  10, "flower":  "R12", "total":  "$12.99", "bundles": {"number": 1, "size": 10 , "price": "$12.99" }},
-        {"number":  15, "flower":  "L09", "total":  "$41.90", "bundles": [{"number": 1, "size": 9, "price": "$24.95" }, {"number": 1, "size": 6 , "price": "$16.95" }]},
-        {"number":  13, "flower":  "T58", "total":  "$25.85", "bundles": [{"number": 2, "size": 5, "price": "$9.95" }, {"number": 1, "size": 3 , "price": "$5.95" }]}
-      ]
-    JSON
-  }
-
-  subject(:result) do
-    json_result = FlowerShopApp.call(order)
-    JSON.load(json_result)
+  let(:orders) do
+    [Order.new(number: 10, code: 'R12'),
+     Order.new(number: 15, code: 'L09'),
+     Order.new(number: 13, code: 'T58')]
   end
 
-  it "outputs the correct shipment configuration" do
-    expect(:result).to eq JSON.load(expected_result_json)
+  let(:flowers) do
+    [Flower.new(name: 'Roses', code: 'R12', bundles: [{ "number": 5, "price": 6.99 },
+                                                      { "number": 10, "price": 12.99 }]),
+     Flower.new(name: 'Lilies', code: 'L09', bundles: [{ "number": 3, "price": 9.95 },
+                                                       { "number": 6, "price": 16.95 },
+                                                       { "number": 9, "price": 24.95 }]),
+     Flower.new(name: 'Tulips', code: 'T58', bundles: [{ "number": 3, "price": 5.95 },
+                                                       { "number": 5, "price": 9.95 },
+                                                       { "number": 9, "price": 16.99 }])]
+  end
+
+  let(:expected_result) do
+    [
+      { "number": 10, "flower":  'R12', "total":  '$12.99',
+        "bundles": [{ "bunches": 1, "size": 10, "price": '$12.99' }] },
+      { "number": 15, "flower":  'L09', "total":  '$41.90',
+        "bundles": [{ "bunches": 1, "size": 9, "price": '$24.95' }, { "bunches": 1, "size": 6, "price": '$16.95' }] },
+      { "number": 13, "flower":  'T58', "total":  '$25.85',
+        "bundles": [{ "bunches": 2, "size": 5, "price": '$19.90' }, { "bunches": 1, "size": 3, "price": '$5.95' }] }
+    ]
+  end
+
+  subject(:result) do
+    FlowerShopApp.call(orders, flowers)
+  end
+
+  it 'outputs the correct shipment configuration' do
+    expect(result).to eq(expected_result)
   end
 end
