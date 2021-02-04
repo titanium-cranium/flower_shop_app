df9dade1113b950c4973a3992749170b43b9a37a
Brendan O'Brien <titaniumcranium@icloud.com>
2021-02-05 04:04:33 +1100

    updated order_processor and spec to use Money for price
    removed specs for now private methods

--- lib/order_processor.rb
+++ lib/order_processor.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require "money"
 
 class OrderProcessor
   attr_reader :order, :flower
@@ -17,6 +18,8 @@ class OrderProcessor
       bundles: bundles_with_cost[:bundles] }
   end
 
+  private
+
   def create_shipment
     bundle_sizes = flower.bundles.collect { |bundle| bundle[:number] }
     ShipmentGenerator.generate_shipment(number: order.number, sizes: bundle_sizes)
@@ -28,13 +31,13 @@ class OrderProcessor
       bundle_cost(bundle)
       bundles << bundle
     end
-    total = bundles.reduce(0) { |sum, bundle| sum + bundle[:price] }
-    { total: total.round(2), bundles: bundles }
+    total = bundles.reduce(0) { |sum, bundle| sum + bundle[:price] }.round.format
+    { total: total, bundles: bundles.each {|bundle| bundle[:price] = bundle[:price].round.format} }
   end
 
   def bundle_cost(bundle)
     bundle_cost = flower.bundles.select { |b| b[:number] == bundle[:size] }.first[:price]
-    bundle[:price] = bundle_cost * bundle[:bunches]
+    bundle[:price] = Money.new(bundle_cost * bundle[:bunches] * 100, "AUD")
     bundle
   end
 end
--- spec/unit/order_processor_spec.rb
+++ spec/unit/order_processor_spec.rb
@@ -21,69 +21,12 @@ RSpec.describe OrderProcessor do
     end
 
     let(:result) do
-      { number: 13, flower: 'T58', "total": 25.85, "bundles": [{ "bunches": 2, "size": 5, "price": 19.90 }, { "bunches": 1, "size": 3, "price": 5.95 }] }
+      { number: 13, flower: 'T58', "total": "$25.85", "bundles": [{ "bunches": 2, "size": 5, "price": "$19.90" }, { "bunches": 1, "size": 3, "price": "$5.95" }] }
     end
 
     describe 'given an order and flower' do
       it 'processes the order' do
         expect(op.process).to eq(result)
-      end
-    end
-  end
-
-  describe '#total_cost' do
-    let(:order) { Order.new(number: 13, code: 'T58') }
-
-    let(:flower) do
-      Flower.new(name: 'Tulips', code: 'T58', bundles: [
-                   { "number": 3, "price": 5.95 },
-                   { "number": 5, "price": 9.95 },
-                   { "number": 9, "price": 16.99 }
-                 ])
-    end
-
-    let(:shipment) do
-      { shipment: [{ size: 5, bunches: 2 }, { size: 3, bunches: 1 }], remainder: 0 }
-    end
-
-    let(:op) do
-      OrderProcessor.new(order: order, flower: flower)
-    end
-
-    let(:result) do
-      { "total": 25.85, "bundles": [{ "bunches": 2, "size": 5, "price": 19.90 }, { "bunches": 1, "size": 3, "price": 5.95 }] }
-    end
-
-    context 'given a shipment with multiple bundles' do
-      it 'appends the total cost' do
-        expect(op.total_cost(shipment)).to eq(result)
-      end
-    end
-  end
-
-  describe '#bundle_cost' do
-    let(:order) { Order.new(number: 13, code: 'T58') }
-
-    let(:flower) do
-      Flower.new(name: 'Roses', code: 'R12', bundles: [
-                   { "number": 5, "price": 6.99 },
-                   { "number": 10, "price": 12.99 }
-                 ])
-    end
-    let(:bundle) do
-      { size: 10, bunches: 1 }
-    end
-    let(:result) do
-      { "bunches": 1, "size": 10, "price": 12.99 }
-    end
-
-    let(:op) do
-      OrderProcessor.new(order: order, flower: flower)
-    end
-
-    context 'given a flower and a bundle size' do
-      it 'appends the correct price' do
-        expect(op.bundle_cost(bundle)).to eq(result)
       end
     end
   end
